2537551173517d2412e0c1d2f65da37f603b98ac
muh-arifulislam <arifibnenam@gmail.com>
2025-10-04 23:33:11 +0600

    blog module updated

--- src/app/modules/blog/blog.service.ts
+++ src/app/modules/blog/blog.service.ts
@@ -1,52 +1,17 @@
 import { Request } from 'express';
 import { Blog } from './blog.model';
+import { QueryBuilder } from '../../utils/QueryBuilder';
 
 const getBlogsFromServer = async (req: Request) => {
-  const { searchTerm, category } = req.query;
-  const page = Number(req.query?.page) || 1;
-  const limit = Number(req.query?.limit) || 9;
-  const skip = (page - 1) * limit;
-  let query = {};
+  const query = new QueryBuilder(Blog.find(), req.query).sort().paginate();
 
-  if (searchTerm) {
-    query = {
-      $or: [
-        { title: { $regex: searchTerm, $options: 'i' } },
-        { subTitle: { $regex: searchTerm, $options: 'i' } },
-      ],
-    };
-  }
+  const data = await query.build();
 
-  if (category?.length) {
-    query = {
-      category: category,
-      ...query,
-    };
-  }
-
-  const products = await Blog.find(query)
-    .skip(skip)
-    .limit(Number(limit))
-    .populate({
-      path: 'user',
-      select: {
-        _id: true,
-        firstName: true,
-        lastName: true,
-      },
-    });
-
-  const total = await Blog.countDocuments(query);
+  const meta = await query.getMeta();
 
   return {
-    data: products,
-    meta: {
-      total,
-      page,
-      limit,
-      skip,
-      hasMore: skip + limit < total,
-    },
+    meta,
+    data,
   };
 };
 
